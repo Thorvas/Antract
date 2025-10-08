a811a295f232201d64df01df3303a765d72d057f
Thorvas <cygan.kacper@outlook.com>
2025-10-08 19:21:57 +0200

    [Backend] Spring Boot project initialized

---
+++ backend/routing-service/antract/src/main/java/com/antract/antract/AntractApplication.java
@@ -0,0 +1,13 @@
+package com.antract.antract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntractApplication.class, args);
+	}
+
+}
---
+++ backend/routing-service/antract/src/test/java/com/antract/antract/AntractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antract.antract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
